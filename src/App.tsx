1b2aeda27707473fa5582e26dbf957130266a9f5
Vlad Burtsev <burthev04@gmail.com>
2024-02-12 16:49:31 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useEffect } from "react";
 import AppRouter from "./routes/AppRouter";
 import { useGetTestQuery } from "./services/vacanciesAPI";
 // import { useGetTestMutation } from "./services/vacanciesAPI";
 
 const App = () => {
-  // const [trigger, { data }] = useGetTestMutation();
-  // useEffect(() => {
-  //   trigger();
-  // }, []);
   const { data } = useGetTestQuery("");
   console.log(data);
 
